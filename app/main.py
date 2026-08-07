e34de5fb79e44c8b2ea248c4f639903c838428b8
SUMANTH M T <sumanthsumanth1966@gmail.com>
2026-08-07 11:54:57 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+
 from models.course import Course
 from routers.courses import router as course_router
 from app.routers import auth
+from app.routers import attendance
 
 app = FastAPI(
     title="School Management System API",
@@ -10,6 +12,7 @@ app = FastAPI(
 
 app.include_router(auth.router)
 app.include_router(course_router)
+app.include_router(attendance.router)
 
 @app.get("/")
 def root():
