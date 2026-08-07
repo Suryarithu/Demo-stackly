301a2eaf57ef927b88a01b91fb18937346a31e13
SUMANTH M T <sumanthsumanth1966@gmail.com>
2026-08-07 11:43:51 +0530

    Update attendance.py

--- app/schemas/attendance.py
+++ app/schemas/attendance.py
@@ -1,1 +1,36 @@
-# Placeholder for Attendance schema
+from datetime import date, datetime
+from pydantic import BaseModel, ConfigDict, Field
+
+class AttendanceBase(BaseModel):
+    student_id: int = Field(
+        gt=0,
+        examples=[1]
+    )
+
+    course_id: int = Field(
+        gt=0,
+        examples=[1]
+    )
+
+    attendance_date: date = Field(
+        examples=["2026-08-07"]
+    )
+
+    status: str = Field(
+        examples=["Present"]
+    )
+
+class AttendanceCreate(AttendanceBase):
+    pass
+
+class AttendanceResponse(AttendanceBase):
+    id: int
+    created_at: datetime | None = None
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+
+
+
+
+
